31c19c771f2bda612980f2d42760df3268bc788e
samiksha <samikshachougale0205@gmail.com>
2025-06-15 16:44:45 +0530

    first commit

---
+++ src/pages/CoursesPage.jsx
@@ -0,0 +1,16 @@
+const CoursesPage = () => {
+    return(
+        <div>
+            <h2>Our Academic Programs</h2>
+            <p>Vivekanand College offers a comprehensive range of undergraduate and postgraduate<br></br> programs designed to equip students with the knowledge and skills demanded by today's <br></br>dynamic global landscape. Our curriculum is regularly updated to reflect industry trends and <br></br> academic advancements.</p>
+             <h3>Discover Campus Life</h3>
+             <hr></hr>
+              <video src="C:\Users\Samiksha\Downloads\college-tour-FAmcnyfF.mp4"  controls>
+              </video>
+
+             
+                </div>
+    )
+}
+
+export default CoursesPage;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header/Header";
+
+const HomePage = () => {
+    return(
+        <div>
+            <Header/>
+            <h1>Welcome to Vivekanand College!</h1>
+            <h3>Your journey to excellence starts here.</h3>
+           
+            <p>This is my first project</p>
+
+        </div>
+    )
+}
+
+export default HomePage;
